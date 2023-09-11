f82d218c7e687f001765b8ef5933000e142f5ddd
BK201-Drama <1243532812@qq.com>
2023-09-11 21:14:44 +0800

    chore(vite): add package && autoImport && env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  envDir: './configs/env',
+  plugins: [
+    react(),
+    autoImport({
+      imports: ['react', 'mobx', 'ahooks'],
+      dts: 'configs/autoImport/auto-imports.d.ts', // 生成自动导入文件声明
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './configs/autoImport/.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+  ],
 })
